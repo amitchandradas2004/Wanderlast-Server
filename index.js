eeda663d9de2d803c227b3b45d595d04374543e6
amitchandradas2004 <amitcdas792@gmail.com>
2026-05-25 22:05:47 +0600

    Destination details page added

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require("express");
 
 const dotenv = require("dotenv");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 dotenv.config();
 const uri = process.env.MONGODB_URI;
 const app = express();
@@ -32,6 +32,13 @@ async function run() {
       res.json(result);
     });
 
+    app.get(`/destination/:id`, async (req, res) => {
+      const { id } = req.params;
+      const result = await destinationCollection.findOne({
+        _id: new ObjectId(id),
+      });
+      res.json(result)
+    });
     app.post("/destination", async (req, res) => {
       const destinationData = req.body;
       console.log(destinationData);
